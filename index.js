bdd547529aff341a44f194313a60d4f37fb00fb4
Vlad Babaev <67766267+Hydro-Dog@users.noreply.github.com>
2021-01-31 17:24:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 
+fs.readFile("aaa.jpg", (err, data) => console.log("fs"));
+setTimeout(() => console.log("setTimeout"), 0);
+setImmediate(() => console.log("setImmediate"));
+
 let eventLoopCounter = 0;
 
 setImmediate(function incrementEventLoopCounter() {
   eventLoopCounter++;
   console.log("eventLoopCounter: ", eventLoopCounter);
-  if (eventLoopCounter < 10) {
+  if (eventLoopCounter < 15) {
     setImmediate(incrementEventLoopCounter);
   }
 });
-
-fs.readFile("aaa.jpg", (err, data) => console.log("fs"));
-setTimeout(() => console.log("setTimeout"), 0);
-setImmediate(() => console.log("setImmediate"));
